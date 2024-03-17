13b077fb006084c23f026f6c283fdd13d4c4ddad
Luke Hunter <luke.hunter9@gmail.com>
2024-03-17 21:13:40 +1100

    updated modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,12 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -20,7 +21,8 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    event.preventDefault();
+    
 }
 
 // Todo: create a function to handle deleting a task
