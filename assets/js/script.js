9ef1339eb06656bc984972ad141127531890b89e
Luke Hunter <luke.hunter9@gmail.com>
2024-03-17 21:22:58 +1100

    Created generateTaskId function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,11 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    if (taskList) {
+        return taskList[taskList.lenghth - 1].id + 1
+    } else {
+        return 1;
+    }
 }
 
 // Todo: create a function to create a task card
