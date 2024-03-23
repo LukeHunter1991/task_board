f89b35a7c2a6b27d181a23de34191455f3a6a1fa
Luke Hunter <luke.hunter9@gmail.com>
2024-03-23 18:50:51 +1100

    styling updates and day js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,31 @@ const $inProgressEl = $('#in-progress-cards').addClass('.lane');
 const $doneEl = $('#done-cards').addClass('.lane');
 
 
-// Todo: create a function to generate a unique task id
+// Creates a function to generate a unique task id
 function generateTaskId() {
+    // As the nextID is assigned 0 if there is nothing in local storage, the next id can always be the curent ID plus 1.
         nextId++
-        // NextID + 1
         localStorage.setItem('nextId', nextId);
 }
 
-// Todo: create a function to create a task card
+// Creates a function to create a task card
 function createTaskCard(task) {
     // Set up task card from object values.
-    const $cardEl = $('<div>').addClass('card dragbox').attr('id', task.id);
+    const $cardEl = $('<div>').addClass('card dragbox').attr('id', task.id).css('margin', '5%');
     const $cardBodyEl = $('<div>').attr('class', 'card-body');
     const $cardTitleEl = $('<h5>').attr('class', 'card-header');
     const $cardTextEl = $('<p>').attr('class', 'card-text');
     const $cardDate = $('<p>').attr('class', 'card-date');
     const $cardButtonEl = $('<a>').attr('href', '#').attr('class', 'btn btn-primary deleteBtn').text('Delete').attr('id', task.id);
 
-    
-    $cardTitleEl.text(task.title);
+    // Uses cardDue function to return the class depending on if the task is overdue, due today, or due in future. 
+    const cardBackground = cardDue(task.date);
 
+    // Adds class for CSS styling based on due status/ 
+    $cardEl.addClass(cardBackground);
+
+    // Push relevant text from the task object into relevant elements.
+    $cardTitleEl.text(task.title);
     $cardTextEl.text(task.taskData);
     $cardDate.text(task.date);
 
@@ -41,7 +46,7 @@ function createTaskCard(task) {
         
 }
 
-// Todo: create a function to render the task list and make cards draggable
+// Creates a function to render the task list and make cards draggable
 function rendertaskList() {
 
     // Grabs tasks from local storage.
@@ -52,8 +57,12 @@ function rendertaskList() {
     $inProgressEl.empty();
     $doneEl.empty();
 
+    // Loops through all tasks in local storage.
     for (items of taskList) {
+        // Build and return card elements
         card = createTaskCard(items);
+
+        // Loads tasks into the current lanes.
         if (items.status == 'to-do') {
             $toDoEl.append(card);
         } else if (items.status == 'in-progress') {
@@ -139,7 +148,7 @@ function handleDeleteTask(event){
 
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+// Creates a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
   // Get id from dropped element.
@@ -161,7 +170,26 @@ function handleDrop(event, ui) {
   rendertaskList();
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+function cardDue(cardDate) {
+    // Create dayjs objects for the current date, and the task date ont he card.
+    const today = dayjs();
+    const date1 = dayjs(cardDate);
+
+    // Get difference between current date and task date.
+    const dateDiff = today.diff(date1, 'd', true);
+
+    // Based on difference between dates, return class for CSS styling.
+    if (dateDiff > 1) {
+        return 'overdue';
+    } else if (dateDiff > 0 && dateDiff < 1) {
+        return 'due';
+    } else {
+        return 'current';
+    };
+
+}
+
+// When the page loads, renders the task list, adds event listeners, makes lanes droppable, and makes the due date field a date picker.
 $(document).ready(function () {
     // Add event listener for submit button to execute handleAddTask function.
     $('#saveBtn').on('click', handleAddTask);
@@ -169,15 +197,18 @@ $(document).ready(function () {
     // Add event listener to container. Only triggered if delete button is clicked.
     $('.container').on('click', '.deleteBtn', handleDeleteTask);
 
+    // Add date picker to modal.
     $( "#inputDate" ).datepicker({
         changeMonth: true,
         changeYear: true,
     });
 
+    // Make lanes droppable.
     $('.lane').droppable({
         accept: '.dragbox',
         drop: handleDrop,
       });
 
+      // Render tasks from local storage on page load.
       rendertaskList()
 });
